351603d8e4533dba10eee9297f1a9f1ca8cc8094
Mike Buonomo <mrb2590@gmail.com>
2021-08-15 18:11:50 +0000

    Update mix configuration

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,13 +11,15 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').vue()
-    .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
-        require('tailwindcss'),
-    ])
-    .webpackConfig(require('./webpack.config'));
+mix
+  .js('resources/js/app.js', 'public/js')
+  .vue()
+  .postCss('resources/css/app.css', 'public/css', [require('postcss-import'), require('tailwindcss')])
+  .webpackConfig(require('./webpack.config'))
+  .extract(['vue']);
 
 if (mix.inProduction()) {
-    mix.version();
+  mix.version();
+} else {
+  mix.sourceMaps();
 }
